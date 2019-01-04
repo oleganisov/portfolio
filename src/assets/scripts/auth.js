aa90b916a4bdd0375462441952293b0c6b3a5ba3
Oleg V. Anisov <anisov@obninsk.ru>
2019-01-05 02:46:30 +0300

    create flip container

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,1 +1,11 @@
-console.log("auth module loaded");
+import switchCards from './modules/switchCards';
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let linkAuth = document.querySelector('#to-auth'),
+        flipContainer = document.querySelector('#flip-container'),
+        signInReset = document.querySelector('#log-reset');
+    
+    //Swipping cards 'welcome' an 'authorization'
+    switchCards(linkAuth, flipContainer, signInReset);
+});
